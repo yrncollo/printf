2bbf0d826da8ea5de7b35c6484b6ac77ba3d860c
YrnCollo <kngenocollins@gmail.com>
2022-07-14 18:27:49 +0300

    eddited main.h file

--- main.h
+++ main.h
@@ -1,10 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <limits.h>
-#include <stdarg.h>
+/*#include <limits.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <unistd.h>*/
 #include <stdarg.h>
 
 /**
@@ -36,20 +35,20 @@ int print_R(va_list R);
 
 
 /* Handlers */
-unsigned char handle_flags(const char *flags);
+/* unsigned char handle_flags(const char *flags);
 unsigned char handle_length(const char *modifier);
 char handle_width(va_list args, const char *modifier, char *index);
 char handle_precision(va_list args, const char *modifier, char *index);
 unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,\
 		unsigned char, char, char, unsigned char);
-
+*/
  /* Helper Functions */
-buffer_t *init_buffer(void);
+/*buffer_t *init_buffer(void);
 void free_buffer(buffer_t *output);
 unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n);
 unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 		unsigned char flags, int wid, int prec);
 unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
 		unsigned char flags, int wid, int prec);
-
+*/
 #endif  /* _MAIN_H */
