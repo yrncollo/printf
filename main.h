c38a0bd3133fa0e390a10ccb8b5dfb6ba330786d
YrnCollo <kngenocollins@gmail.com>
2022-07-14 18:31:27 +0300

    eddited main.h file

--- main.h
+++ main.h
@@ -1,11 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
-/*#include <limits.h>
-#include <stdlib.h>
-#include <unistd.h>*/
 #include <stdarg.h>
-
 /**
  * struct print - structure for printing various types
  * @t: type to print
@@ -17,8 +15,8 @@ typedef struct print
 	int (*f)(va_list);
 } print_t;
 
-int _printf(const char *format, ...);
 int _putchar(char c);
+int _printf(const char *format, ...);
 int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);
@@ -33,22 +31,4 @@ int print_S(va_list S);
 int print_r(va_list r);
 int print_R(va_list R);
 
-
-/* Handlers */
-/* unsigned char handle_flags(const char *flags);
-unsigned char handle_length(const char *modifier);
-char handle_width(va_list args, const char *modifier, char *index);
-char handle_precision(va_list args, const char *modifier, char *index);
-unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,\
-		unsigned char, char, char, unsigned char);
-*/
- /* Helper Functions */
-/*buffer_t *init_buffer(void);
-void free_buffer(buffer_t *output);
-unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n);
-unsigned int convert_sbase(buffer_t *output, long int num, char *base,
-		unsigned char flags, int wid, int prec);
-unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
-		unsigned char flags, int wid, int prec);
-*/
 #endif  /* _MAIN_H */
